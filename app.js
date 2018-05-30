e2bf84fc774ffc872bd347b9f1f28af981d70920
Baldrani <mael.mayon@free.fr>
2018-05-30 22:44:16 +0200

    Kind of state but style buggy

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var io = require('socket.io')(http);
 
 app.get('/level/*', function(req, res){
     var nsp = io.of(req.originalUrl);
+    console.log(req.originalUrl);
     nsp.on('connection', function(socket){
-        console.log('someone connected');
-        nsp.emit('hi', 'everyone!');
+        socket.broadcast.emit('connection', 'user x connected !'); //Envoi a tous que l'utilisateur s'est connecté
+        socket.on('sendMessage', function(msg){
+            nsp.emit('message', msg);
+            console.log('message: ' + msg);
+        });
+        socket.on('disconnect', function(){
+            nsp.emit('message', "user disconnected");
+            console.log('user disconnected');
+        });
     });
     res.sendFile(__dirname + '/index.html');
 })
 
-
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(2000, function(){
+    console.log('listening on *:2000');
 });
