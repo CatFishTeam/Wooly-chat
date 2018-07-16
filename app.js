d8af0f0cfae76f4dc8415025b0f51dce18fd6a1f
Baldrani <mael.mayon@free.fr>
2018-07-16 13:49:44 +0200

    Add UserName

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get('/level/*', function(req, res){
     nsp = io.of(req.originalUrl);
     nsp.on('connection', function(socket){
         if (!socket.sentMydata) {
-            socket.broadcast.emit('connection', 'user x connected !'); //Envoi a tous que l'utilisateur s'est connecté
+            console.log(parseCookies(req).wooly_user_name);
+            socket.broadcast.emit('connection', parseCookies(req).wooly_user_name != undefined ? wooly_user_name : 'Anonyme' + ' connected !'); //Envoi a tous que l'utilisateur s'est connecté
             socket.on('sendMessage', function(msg){
                 nsp.emit('message', msg);
             });
@@ -22,3 +23,15 @@ app.get('/level/*', function(req, res){
 http.listen(2000, function(){
     console.log('listening on *:2000');
 });
+
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
